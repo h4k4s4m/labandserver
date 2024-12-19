10c044ceec67a54cce3492afb8812926136ff6e1
Sahm Samarghandi <sahm.samarghandi@gmail.com>
2024-12-19 13:50:18 -0800

    t

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/quick_start/intro">
+            to="/quick_start/getting_started/labs-and-servers">
             Quick Start ⚡️
           </Link>
         </div>
